2d3668e370170d8525861338267892ddd6170cde
league <none>
2024-07-14 13:00:36 -0700

    Finished all, next assignment is part #3C

--- _02_code_flow/_c_flaming_ninja_star.py
+++ _02_code_flow/_c_flaming_ninja_star.py
@@ -21,51 +21,49 @@ if __name__ == '__main__':
     flameSize = 130         # the length of the flaming arms
     
     # Make a new turtle
-    turtle=turtle.Turtle()
+    murtle=turtle.Turtle()
     # Make the turtle shape 'turtle', .shape('turtle')
-    turtle.shape('turtle')
+    murtle.shape('turtle')
     # Set the turtle width to 2
-    turtle.width(2)
+    murtle.width(2)
     # Set the turtle speed to 0 (fastest)
-    turtle.speed(0)
+    murtle.speed(0)
     # Use a for loop to repeat all of the code below ONE time (we will change
     # this later)
-    for i in range(1):
+    for i in range(25):
         # Set the turtle .fillcolor() to orange
-        turtle.fillcolor('orange')
+        murtle.fillcolor('orange')
         # Call the turtle .begin_fill() function
-        turtle.begin_fill()
+        murtle.begin_fill()
         # TURN RIGHT     Turn the turtle 1/8 of a circle (hint: 360 degrees
         #                will turn a full circle)
-        turtle.right(45)
+        murtle.right(45)
         # DRAW           Move the turtle 64 pixels
-        turtle.forward(64*i)
+        murtle.forward(64)
         # TURN LEFT      Turn the turtle 40 degrees to the LEFT. (Negative
         #                numbers will turn the turtle counter-clockwise.)
-        turtle.left(40)
+        murtle.left(40)
         # DRAW FLAME     Move the turtle the distance in the variable flameSize
-        turtle.forward(flameSize)
+        murtle.forward(flameSize)
         #                Turn the turtle to the right 170 degrees
-        turtle.right(170)
+        murtle.right(170)
         #                Move the turtle the distance in the variable flameSize (again)
-        turtle.forward(flameSize)
+        murtle.forward(flameSize)
         #  TURN RIGHT    Turn the turtle 62 degrees to the right
-        turtle.right(62)
+        murtle.right(62)
         #  DRAW          Move the turtle the distance in the variable baseSize
-        turtle.forward(baseSize)
+        murtle.forward(baseSize)
         # Call the turtle .end_fill() method
-        turtle.end_fill()
+        murtle.end_fill()
     # Hide your turtle so you can see the pattern.
-        turtle.hideturtle()
+        murtle.hideturtle()
     # TEST   Run the program. Check that your shape is the same as the first
     #        picture in the recipe. This is one arm of the ninja star.
 
     # COLOR  Change the turtle's pen color so that the flame is a different
     #        color to the rest of the star. Run the program again. Check the
     #        second picture in the recipe.
-
     # LOOP   When you have one arm looking right, change your for loop to
     #        repeat 25 times.
-    
     # call the turtle .done() method
-    #turtle.done()
+    turtle.done()
--- _03_print_and_popups/_b_mad_libs.py
+++ _03_print_and_popups/_b_mad_libs.py
@@ -3,12 +3,13 @@ from tkinter import messagebox, simpledialog, Tk
 # Create an if-main code block, *hint, type main then ctrl+space to auto-complete
 if __name__ == '__main__':
     # Make a new window variable, window = Tk()
-
+    window = Tk()
     # Hide the window using the window's .withdraw() method
-
+    window.withdraw()
     # Put this sentence in a pop-up message box:
     # "If you find yourself having to cross a piranha-infested river, here's how to do it..."
-
+    simpledialog.askstring(title='Greeter', prompt='If you find yourself having to cross a piranha-infested river,\n '
+                                                   'here is how to do it...')
     # Get the player to enter an adjective
 
     # Get the player to enter a type of liquid
@@ -19,28 +20,32 @@ if __name__ == '__main__':
 
     # Get the player to enter a place
 
-    # The story below has has been written as a group of Strings joined
+    # The story below has been written as a group of Strings joined
     # together by + signs. The story contains place holders, indicated
     # by [** **] which you need to replace with the values entered by the
     # player.
     # Hint: You will need to add more + signs to join the variables to the
     #       other parts of the story.
-
+    adjective='aggressive'
+    liquid='water'
+    body_part='leg'
+    verb='swimming'
+    place='shore'
     story = (
-        "Piranhas are more [**adjective**] during the day, so cross the river at\n"
-        "night. Piranhas are attracted to fresh [**type of liquid**] and will most\n"
-        "likely take a bite out of your [**body part**] if you [**verb**]. Whatever\n"
-        "you do, if you have an open wound, try to find another way to get "
-        "back to the [**place**]. Good luck!"
+        f"Piranhas are more {'aggressive'} during the day, so cross the river at\n"
+        f"night. Piranhas are attracted to fresh {'water'} and will most\n"
+        f"likely take a bite out of your {'leg'} if you {'swimming'}. Whatever\n"
+        "you do, if you have an open wound, try to find another way to get\n "
+        f"back to the {'shore'}. Good luck!"
     )
 
     # Make a pop-up that contains the final story. The \n escape characters add
     # line breaks to the story. If you need to, move them around to make your
     # story look better in the pop-up
-
+    simpledialog.askstring(title='Greeter', prompt=story)
     # If you want to write your own Madlib story, just change the story variable
     # and ask the player different questions.
 
     # Run the window's .mainloop() method
-
+    window.mainloop()
     pass
